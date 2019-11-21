b79da67125434756558681c726776bf04ea22528
ninn <toyninn@gmail.com>
2019-11-21 23:30:32 +0800

    Login & sinup app useing error handling

---
+++ week3/LoginSignup/LoginSignup/InvalidError.swift
@@ -0,0 +1,9 @@
+//
+//  InvalidError.swift
+//  LoginSignup
+//
+//  Created by Ninn on 2019/11/20.
+//  Copyright © 2019 Ninn. All rights reserved.
+//
+
+import Foundation
---
+++ week3/LoginSignup/LoginSignup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginSignup
+//
+//  Created by Ninn on 2019/11/20.
+//  Copyright © 2019 Ninn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
